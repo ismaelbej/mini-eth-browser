564f0a72152790309cbc4a671d51d18a3e81429d
Ismael Bejarano <ismaelbej@gmail.com>
2017-08-03 00:03:24 -0300

    Enable cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const api = require('./routes/api');
 
@@ -16,6 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/api/v1', api);
 
