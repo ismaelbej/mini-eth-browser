b9f0910ef7314a42218266c79c648866818f84fc
Ismael Bejarano <ismaelbej@gmail.com>
2017-08-03 00:21:20 -0300

    Implement block end point

--- routes/block.js
+++ routes/block.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const ethereum = require('../lib/ethereum');
 
 const router = express.Router();
 
@@ -6,12 +7,18 @@ router.get('/', function (req, res) {
   res.json({ error: 'Missing block hash' });
 });
 
-router.get('/:hash', function (req, res) {
-  res.json({
-    block: {
-      hash: req.params.hash,
-    }
-  });
+router.get('/:hash', async function (req, res) {
+  try {
+    blockInfo = await ethereum.getBlockInfo(req.params.hash);
+    res.json({
+      block: blockInfo,
+    });
+  } catch (err) {
+    res.status(err.status || 500);
+    res.json({
+      errors: [ err.message ]
+    });
+  }
 });
 
 module.exports = router;
