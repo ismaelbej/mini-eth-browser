a0f727064c3e8a6d4ca28c419f45b7e55b7173bb
Ismael Bejarano <ismaelbej@gmail.com>
2017-08-04 04:40:05 -0300

    Fix block view didn't refresh on navigation

--- client/src/views/Block.js
+++ client/src/views/Block.js
@@ -1,21 +1,54 @@
 import React from 'react';
+import {
+  Route,
+  Switch
+} from 'react-router-dom';
 import { getBlockInfo } from '../models/Block';
 import BlockInfo from '../components/BlockInfo';
 
-class Block extends React.Component {
+const BlockList = () => {
+  return (
+    <div>
+      <div className="row">
+        <h1>Block</h1>
+      </div>
+      <div className="row">
+        Loading...
+      </div>
+    </div>);
+}
+
+class BlockView extends React.Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
       block: null,
+      hash: props.match.params.hash,
     };
   }
 
-  async componentDidMount() {
-    const { block } = await getBlockInfo(1);
+  async loadBlockInfo(hash) {
+    const { block } = await getBlockInfo(hash);
     this.setState({
       block,
+      hash
     });
+  }
+
+  componentDidMount() {
+    this.loadBlockInfo(this.state.hash);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.match.params.hash !== nextProps.match.params.hash) {
+      this.setState({
+        block: null,
+        hash: nextProps.match.params.hash
+      });
+      this.loadBlockInfo(nextProps.match.params.hash);
+    }
   }
 
   render() {
@@ -31,5 +64,12 @@ class Block extends React.Component {
       </div>);
   }
 }
+
+const Block = () => (
+  <Switch>
+    <Route exact path="/block" component={BlockList} />
+    <Route path="/block/:hash" component={BlockView} />
+  </Switch>
+);
 
 export default Block;
