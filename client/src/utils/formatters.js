7dd2b0ce33c3b47686fca0c536ebf1733731735e
Ismael Bejarano <ismaelbej@gmail.com>
2017-08-04 17:12:49 -0300

    Add address and hash formatters

--- client/src/utils/formatters.js
+++ client/src/utils/formatters.js
@@ -1,9 +1,19 @@
 import moment from 'moment';
 
+const HASH_LENGTH = 20;
+
 export function formatTimestamp(timestamp) {
   return moment(new Date(timestamp * 1000)).format();
 }
 
+export function formatHash(hash, length = HASH_LENGTH) {
+  return hash.length > length ? hash.substr(0, length - 2) + '..' : hash;
+}
+
+export const formatAddress = formatHash;
+
 export default {
+  formatAddress,
+  formatHash,
   formatTimestamp,
 };
