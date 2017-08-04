25d21797e4cdd2e11f8649f44e3db987ff35a15e
Ismael Bejarano <ismaelbej@gmail.com>
2017-08-04 17:17:16 -0300

    Show block listing if block hash passed

--- client/src/views/Block.js
+++ client/src/views/Block.js
@@ -3,22 +3,68 @@ import {
   Route,
   Switch
 } from 'react-router-dom';
-import { getBlockInfo } from '../models/Block';
+import queryString from 'query-string';
+import {
+  getBlockInfo,
+  getBlockList
+} from '../models/Block';
 import BlockInfo from '../components/BlockInfo';
+import BlockList from '../components/BlockList';
 
-const BlockList = () => {
-  return (
-    <div>
-      <div className="row">
-        <h1>Block</h1>
+class BlockListView extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    const { start, count } = queryString.parse(props.location.search);
+
+    this.state = {
+      start: typeof start !== 'undefined' ? start : -1,
+      count: typeof count !== 'undefined' ? count : 25,
+      blocks: null
+    };
+  }
+
+  async loadBlockList(start, count) {
+    const { blocks } = await getBlockList(start, count);
+    this.setState({
+      blocks
+    });
+  }
+
+  componentDidMount() {
+    this.loadBlockList(this.state.start, this.state.count);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { start, count } = queryString.parse(nextProps.location.search)
+    if (this.state.start !== start ||
+      this.state.count !== count) {
+      this.setState({
+        start: start,
+        count: count,
+        blocks: null
+      });
+      this.loadBlockList(start, count);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <h1>Block</h1>
+        </div>
+        <div className="row">
+          {this.state.blocks && <BlockList blocks={this.state.blocks} />}
+          {!this.state.blocks && 'Loading...'}
+        </div>
       </div>
-      <div className="row">
-        Loading...
-      </div>
-    </div>);
+    );
+  }
 }
 
-class BlockView extends React.Component {
+class BlockInfoView extends React.Component {
 
   constructor(props) {
     super(props);
@@ -61,15 +107,16 @@ class BlockView extends React.Component {
           {this.state.block && <BlockInfo block={this.state.block} />}
           {!this.state.block && 'Loading...'}
         </div>
-      </div>);
+      </div>
+    );
   }
 }
 
-const Block = () => (
+const BlockView = () => (
   <Switch>
-    <Route exact path="/block" component={BlockList} />
-    <Route path="/block/:hash" component={BlockView} />
+    <Route exact path="/block" component={BlockListView} />
+    <Route path="/block/:hash" component={BlockInfoView} />
   </Switch>
 );
 
-export default Block;
+export default BlockView;
