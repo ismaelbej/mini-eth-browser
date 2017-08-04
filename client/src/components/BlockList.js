80057eca4b4dce33450530bf9e39c6edc2d21520
Ismael Bejarano <ismaelbej@gmail.com>
2017-08-04 17:16:06 -0300

    Display block listing

---
+++ client/src/components/BlockList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  formatAddress,
+  formatHash,
+  formatTimestamp
+} from '../utils/formatters';
+
+const BlockInfo = (props) => {
+  const { blocks } = props;
+  return (
+    <table className="u-full-width">
+      <thead>
+        <tr>
+          <th>Hash</th>
+          <th>Block</th>
+          <th>Num. Transactions</th>
+          <th>Date</th>
+          <th>Miner</th>
+        </tr>
+      </thead>
+      <tbody>
+      {blocks.map(block => (
+        <tr key={block.hash}>
+          <td><Link to={`/block/${block.hash}`}>{formatHash(block.hash)}</Link></td>
+          <td><Link to={`/block/${block.hash}`}>{block.number}</Link></td>
+          <td>{block.transactions.length}</td>
+          <td>{formatTimestamp(block.timestamp)}</td>
+          <td><Link to={`/account/${block.miner}`}>{formatAddress(block.miner)}</Link></td>
+        </tr>
+      ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default BlockInfo;
