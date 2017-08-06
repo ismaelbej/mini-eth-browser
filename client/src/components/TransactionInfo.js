6f593cdc0d9fbcb0dfa162332e48d2e3796fc793
Ismael Bejarano <ismaelbej@gmail.com>
2017-08-06 13:58:22 -0300

    Fix warning about tbody missing

--- client/src/components/TransactionInfo.js
+++ client/src/components/TransactionInfo.js
@@ -6,55 +6,57 @@ const TransactionInfo = (props) => {
   const { tx } = props;
   return (
     <table className="u-full-width">
-      <tr>
-        <td>Txid:</td>
-        <td>{tx.hash}</td>
-      </tr>
-      <tr>
-        <td>Block Hash:</td>
-        <td><Link to={`/block/${tx.blockHash}`}>{tx.blockHash}</Link></td>
-      </tr>
-      <tr>
-        <td>Block Number:</td>
-        <td><Link to={`/block/${tx.blockHash}`}>{tx.blockNumber}</Link></td>
-      </tr>
-      <tr>
-        <td>Transaction Index:</td>
-        <td>{tx.transactionIndex}</td>
-      </tr>
-      <tr>
-        <td>From:</td>
-        <td><Link to={`/account/${tx.from}`}>{tx.from}</Link></td>
-      </tr>
-      <tr>
-        <td>To:</td>
-        {tx.to && <td><Link to={`/account/${tx.to}`}>{tx.to}</Link></td>}
-        {!tx.to && 'Contract creation'}
-      </tr>
-      <tr>
-        <td>Gas:</td>
-        <td>{tx.gas}</td>
-      </tr>
-      <tr>
-        <td>Gas Price:</td>
-        <td>{tx.gasPrice}</td>
-      </tr>
-      <tr>
-        <td>Nonce:</td>
-        <td>{tx.nonce}</td>
-      </tr>
-      <tr>
-        <td>Gas Price:</td>
-        <td>{tx.gasPrice}</td>
-      </tr>
-      <tr>
-        <td>Value:</td>
-        <td>{tx.value}</td>
-      </tr>
-      <tr>
-        <td>Input:</td>
-        <td>{tx.input}</td>
-      </tr>
+      <tbody>
+        <tr>
+          <td>Txid:</td>
+          <td>{tx.hash}</td>
+        </tr>
+        <tr>
+          <td>Block Hash:</td>
+          <td><Link to={`/block/${tx.blockHash}`}>{tx.blockHash}</Link></td>
+        </tr>
+        <tr>
+          <td>Block Number:</td>
+          <td><Link to={`/block/${tx.blockHash}`}>{tx.blockNumber}</Link></td>
+        </tr>
+        <tr>
+          <td>Transaction Index:</td>
+          <td>{tx.transactionIndex}</td>
+        </tr>
+        <tr>
+          <td>From:</td>
+          <td><Link to={`/account/${tx.from}`}>{tx.from}</Link></td>
+        </tr>
+        <tr>
+          <td>To:</td>
+          {tx.to && <td><Link to={`/account/${tx.to}`}>{tx.to}</Link></td>}
+          {!tx.to && 'Contract creation'}
+        </tr>
+        <tr>
+          <td>Gas:</td>
+          <td>{tx.gas}</td>
+        </tr>
+        <tr>
+          <td>Gas Price:</td>
+          <td>{tx.gasPrice}</td>
+        </tr>
+        <tr>
+          <td>Nonce:</td>
+          <td>{tx.nonce}</td>
+        </tr>
+        <tr>
+          <td>Gas Price:</td>
+          <td>{tx.gasPrice}</td>
+        </tr>
+        <tr>
+          <td>Value:</td>
+          <td>{tx.value}</td>
+        </tr>
+        <tr>
+          <td>Input:</td>
+          <td>{tx.input}</td>
+        </tr>
+      </tbody>
     </table>
   );
 };
