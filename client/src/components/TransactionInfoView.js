53eb316b2bd415a7749ce42a6b8a2d1b9e2c5e56
Ismael Bejarano <ismaelbej@gmail.com>
2017-08-06 14:12:43 -0300

    Split transaction info query into its own component

---
+++ client/src/components/TransactionInfoView.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  getTransactionInfo
+} from '../models/Transaction';
+import TransactionInfo from './TransactionInfo';
+
+class TransactionInfoView extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    const { txid } = props;
+    this.state = {
+      tx: null,
+      txid
+    };
+  }
+
+  async loadTransactionInfo(txid) {
+    const { tx } = await getTransactionInfo(txid);
+    console.log(tx);
+    this.setState({
+      tx,
+      txid
+    });
+  }
+
+  componentDidMount() {
+    this.loadTransactionInfo(this.state.txid);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { txid } = nextProps;
+    if (this.state.txid !== txid) {
+      this.setState({
+        tx: null,
+        txid
+      });
+      this.loadTransactionInfo(txid);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {!this.state.tx && 'Loading...'}
+        {this.state.tx && <TransactionInfo tx={this.state.tx} />}
+      </div>
+    );
+  }
+}
+
+export default TransactionInfoView;
--- client/src/views/Transaction.js
+++ client/src/views/Transaction.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import {
-  Link,
   Route,
   Switch
 } from 'react-router-dom';
-import {
-  getTransactionInfo
-} from '../models/Transaction';
-import TransactionInfo from '../components/TransactionInfo';
+import TransactionInfoView from '../components/TransactionInfoView';
 
 class TransactionListView extends React.Component {
 
@@ -29,48 +25,13 @@ class TransactionListView extends React.Component {
   }
 }
 
-class TransactionInfoView extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      tx: null,
-      txid: props.match.params.txid
-    };
-  }
-
-  async loadTransactionInfo(txid) {
-    const { tx } = await getTransactionInfo(txid);
-    console.log(tx);
-    this.setState({
-      tx,
-      txid
-    });
-  }
-
-  componentDidMount() {
-    this.loadTransactionInfo(this.state.txid);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (this.state.txid !== nextProps.match.params.txid) {
-      this.setState({
-        tx: null,
-        txid: nextProps.match.params.txid
-      });
-      this.loadTransactionInfo(nextProps.match.params.txid);
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        {!this.state.tx && 'Loading...'}
-        {this.state.tx && <TransactionInfo tx={this.state.tx} />}
-      </div>
-    );
-  }
+const TransactionInfoQuery = (props) => {
+  const { txid } = props.match.params;
+  return (
+    <div>
+      <TransactionInfoView txid={txid} />
+    </div>
+  );
 }
 
 const TransactionView = () => (
@@ -81,7 +42,7 @@ const TransactionView = () => (
     <div className="row">
       <Switch>
         <Route exact path="/tx" component={TransactionListView} />
-        <Route path="/tx/:txid" component={TransactionInfoView} />
+        <Route path="/tx/:txid" component={TransactionInfoQuery} />
       </Switch>
     </div>
   </div>
