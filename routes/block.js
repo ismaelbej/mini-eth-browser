667fd7b8b2d047f9c3d149a29434105e70f0ffa4
Ismael Bejarano <ismaelbej@gmail.com>
2017-08-13 00:37:26 -0300

    Move tx list endpoint to /block/hash/txs

--- routes/block.js
+++ routes/block.js
@@ -6,23 +6,32 @@ const ethereum = require('../lib/ethereum');
 const router = express.Router();
 
 const BLOCK_COUNT = 25;
+const TX_START = 0;
+const TX_COUNT = 25;
+
+async function parseBlockParams(query) {
+  let start = -1;
+  if (typeof query.start === 'string') {
+    start = parseInt(query.start, 10);
+  } else if (typeof query.start === 'undefined') {
+    start = await ethereum.getLatestBlock();
+  }
+  let count = BLOCK_COUNT;
+  if (typeof query.count === 'string') {
+    count = parseInt(query.count, 10);
+  }
+  if (count <= 0) {
+    count = BLOCK_COUNT;
+  }
+  return {
+    start,
+    count,
+  };
+}
 
 router.get('/', async (req, res) => {
   try {
-    let start = -1;
-    if (typeof req.query.start === 'string') {
-      start = parseInt(req.query.start, 10);
-    }
-    if (start < 0) {
-      start = await ethereum.getLatestBlock();
-    }
-    let count = BLOCK_COUNT;
-    if (typeof req.query.count === 'string') {
-      count = parseInt(req.query.count, 10);
-    }
-    if (count <= 0) {
-      count = BLOCK_COUNT;
-    }
+    const { start, count } = await parseBlockParams(req.query);
 
     if (start >= 0) {
       const blocks = await Promise.map(
@@ -50,6 +59,48 @@ router.get('/:hash', async (req, res) => {
     const block = await ethereum.getBlockInfo(req.params.hash);
     res.json({
       block,
+    });
+  } catch (err) {
+    res.status(err.status || 500);
+    res.json({
+      errors: [err.message],
+    });
+  }
+});
+
+async function parseTxParams(query) {
+  let start = TX_START;
+  if (typeof query.start === 'string') {
+    start = parseInt(query.start, 10);
+  }
+  if (start < 0) {
+    start = await ethereum.getLatestBlock();
+  }
+  let count = TX_COUNT;
+  if (typeof query.count === 'string') {
+    count = parseInt(query.count, 10);
+  }
+  if (count <= 0) {
+    count = TX_COUNT;
+  }
+  return {
+    start,
+    count,
+  };
+}
+
+router.get('/:hash/txs/', async (req, res) => {
+  try {
+    const { start, count } = await parseTxParams(req.query);
+    const max = await ethereum.getBlockTransactionCount(req.params.hash);
+    const txrange = _.range(start, _.min([start + count, max]));
+    const txs = await Promise.all(
+      txrange.map(idx =>
+        ethereum.getTransactionFromBlock(req.params.hash, idx)
+      )
+    );
+    res.json({
+      txs,
     });
   } catch (err) {
     res.status(err.status || 500);
--- routes/tx.js
+++ routes/tx.js
@@ -5,56 +5,11 @@ const router = express.Router();
 
 const TX_COUNT = 10;
 
-function parseParams(query) {
-  let block;
-  if (typeof query.block === 'string') {
-    if (query.block.length > 0) {
-      block = query.block;
-    }
-  }
-  let start = 0;
-  if (typeof query.start === 'string') {
-    start = parseInt(query.start, 10);
-  }
-  if (start < 0) {
-    start = 0;
-  }
-  let count = TX_COUNT;
-  if (typeof query.count === 'string') {
-    count = parseInt(query.count, 10);
-  }
-  if (count <= 0) {
-    count = TX_COUNT;
-  }
-  return {
-    block,
-    start,
-    count,
-  };
-}
-
 router.get('/', async (req, res) => {
-  try {
-    const { block, start, count } = parseParams(req.query);
-    if (block) {
-      const blockInfo = await ethereum.getBlockInfo(block);
-      const txids = blockInfo.transactions.slice(start, start + count);
-      const txs = await Promise.all(txids.map(ethereum.getTransactionInfo));
-      res.json({
-        txs,
-      });
-    } else {
-      const txs = await ethereum.getPendingTransactions();
-      res.json({
-        txs,
-      });
-    }
-  } catch (err) {
-    res.status(err.status || 500);
-    res.json({
-      errors: [err.message],
-    });
-  }
+  res.status(err.status || 500);
+  res.json({
+    errors: [err.message],
+  });
 });
 
 router.get('/:txid', async (req, res) => {
