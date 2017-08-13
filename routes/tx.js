30398bf178a07bb155a5dc18a84b4513f92a7f8a
Ismael Bejarano <ismaelbej@gmail.com>
2017-08-13 04:51:57 -0300

    Remove unused endpoint

--- routes/tx.js
+++ routes/tx.js
@@ -3,15 +3,6 @@ const ethereum = require('../lib/ethereum');
 
 const router = express.Router();
 
-const TX_COUNT = 10;
-
-router.get('/', async (req, res) => {
-  res.status(err.status || 500);
-  res.json({
-    errors: [err.message],
-  });
-});
-
 router.get('/:txid', async (req, res) => {
   try {
     const tx = await ethereum.getTransactionInfo(req.params.txid);
