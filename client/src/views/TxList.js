e8f7b63cb21029162c72e720f4a217d5d87b508e
Ismael Bejarano <ismaelbej@gmail.com>
2017-10-16 04:38:35 -0300

    Move controller into transaction list view

--- client/src/views/TxList.js
+++ client/src/views/TxList.js
@@ -3,12 +3,23 @@ import {
   Link,
 } from 'react-router-dom';
 import {
+  Button,
   Grid,
   Header,
+  Loader,
+  Table,
 } from 'semantic-ui-react';
 import queryString from 'query-string';
 import TxListComponent from '../components/TxList';
-import TxListController from '../controllers/TxList';
+import {
+  getBlockInfo,
+  getTransactionList,
+} from '../lib/api';
+import { formatTimestamp } from '../utils/formatters';
+
+
+const TX_COUNT = 20;
+
 
 function parseParams(props) {
   const { hash } = props.match.params;
@@ -33,55 +44,57 @@ function parseParams(props) {
 class TxList extends React.Component {
   constructor(props) {
     super(props);
-    const { hash, start, count } = parseParams(props);
-    this.state = {
-      hash,
-      start,
-      count,
-      txs: [],
-      nextTx: undefined,
-      prevTx: undefined,
-    };
+    this.state = {};
   }
 
   componentDidMount() {
-    this.controller = new TxListController();
-    this.controller.on('txs', txs => this.handleTxs(txs));
-    this.controller.on('fail', err => this.handleFailure(err));
-    this.controller.initialize(this.state.hash, this.state.start, this.state.count);
+    const { hash, start, count } = parseParams(this.props);
+    this.loadData(hash, start, count);
   }
 
   componentWillReceiveProps(nextProps) {
     const { hash, start, count } = parseParams(nextProps);
     if (this.state.hash !== hash ||
-      this.state.start !== start ||
-      this.state.count !== count) {
-      this.setState({
-        hash,
-        start,
-        count,
-      });
-      this.controller.loadTxList(hash, start, count);
+        this.state.start !== start ||
+        this.state.count !== count) {
+      this.loadData(hash, start, count);
     }
   }
 
-  handleTxs({ txs, nextTx, prevTx }) {
-    this.setState({
-      txs,
-      nextTx,
-      prevTx,
-    });
-  }
-
-  handleFailure(err) {
-    this.setState({
-      txs: [],
-      nextTx: undefined,
-      prevTx: undefined,
-    });
+  async loadData(hash, start = 0, count = TX_COUNT) {
+    try {
+      this.setState({ loading: true, error: false });
+      const [{ txs }, { block }] = await Promise.all([
+        getTransactionList(hash, start, count),
+        getBlockInfo(hash),
+      ]);
+      const data = {
+        hash,
+        start,
+        count,
+        block,
+        txs,
+        nextTx: start + txs.length < block.transactions.length ? start + txs.length : -1,
+        prevTx: start >= count ? start - count : -1,
+      };
+      this.setState({ loading: false, error: false, data });
+    } catch (err) {
+      this.setState({ loading: false, error: true });
+    }
   }
 
   render() {
+    const {
+      loading,
+      data: {
+        hash,
+        block,
+        count,
+        txs = [],
+        nextTx = -1,
+        prevTx = -1,
+      } = {},
+    } = this.state;
     return (
       <Grid>
         <Grid.Row>
@@ -89,16 +102,52 @@ class TxList extends React.Component {
             <Header as="h1">Transactions</Header>
           </Grid.Column>
         </Grid.Row>
-        <Grid.Row>
+        {block && <Grid.Row>
           <Grid.Column>
-            {this.state.prevTx !== false && <Link to={`/block/${this.state.hash}/txs/?start=${this.state.prevTx}${this.state.count ? `&count=${this.state.count}` : ''}`} className="button button-primary">Previous</Link>}
-            {this.state.prevTx !== false && '\u00a0'}
-            {this.state.nextTx && <Link to={`/block/${this.state.hash}/txs/?start=${this.state.nextTx}${this.state.count ? `&count=${this.state.count}` : ''}`} className="button button-primary">Next</Link>}
+            <Table>
+              <Table.Body>
+                <Table.Row>
+                  <Table.Cell>Hash:</Table.Cell>
+                  <Table.Cell>{block.hash}</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>Number:</Table.Cell>
+                  <Table.Cell>{block.number}</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>Date:</Table.Cell>
+                  <Table.Cell>{formatTimestamp(block.timestamp)}</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>Num. Transactions:</Table.Cell>
+                  <Table.Cell>{block.transactions.length}</Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            </Table>
           </Grid.Column>
-        </Grid.Row>
+        </Grid.Row>}
         <Grid.Row>
           <Grid.Column>
-            <TxListComponent txs={this.state.txs} />
+            {loading && <Loader active inline size="tiny" />}
+            <Button.Group floated="right">
+              <Button
+                {...{ disabled: prevTx < 0 }}
+                labelPosition="left"
+                content="Previous"
+                icon="left chevron"
+                as={Link}
+                to={`/block/${hash}/txs/?start=${prevTx}&count=${count}`}
+              />
+              <Button
+                {...{ disabled: nextTx < 0 }}
+                labelPosition="right"
+                content="Next"
+                icon="right chevron"
+                as={Link}
+                to={`/block/${hash}/txs/?start=${nextTx}&count=${count}`}
+              />
+            </Button.Group>
+            <TxListComponent txs={txs} />
           </Grid.Column>
         </Grid.Row>
       </Grid>
