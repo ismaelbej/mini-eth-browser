52139462b29c38767069fa9fd60c546eb7cc120e
Ismael Bejarano <ismaelbej@gmail.com>
2017-10-17 05:01:53 -0300

    Refactor block info

--- client/src/views/BlockInfo.js
+++ client/src/views/BlockInfo.js
@@ -3,59 +3,65 @@ import {
   Link,
 } from 'react-router-dom';
 import {
+  Button,
   Grid,
   Header,
+  Loader,
 } from 'semantic-ui-react';
 import BlockInfoComponent from '../components/BlockInfo';
-import BlockInfoController from '../controllers/BlockInfo';
+import {
+  getBlockchainInfo,
+  getBlockInfo,
+} from '../lib/api';
 
 class BlockInfo extends React.Component {
   constructor(props) {
     super(props);
-    const { hash } = props.match.params;
-    this.state = {
-      hash,
-      block: undefined,
-      nextBlock: undefined,
-      prevBlock: undefined,
-      error: undefined,
-    };
+    this.state = {};
   }
 
   componentDidMount() {
-    this.controller = new BlockInfoController();
-    this.controller.on('block', block => this.handleBlock(block));
-    this.controller.on('fail', err => this.handleFailure(err));
-    this.controller.initialize(this.state.hash);
+    const { hash } = this.props.match.params;
+    this.loadData(hash);
   }
 
   componentWillReceiveProps(nextProps) {
     const { hash } = nextProps.match.params;
     if (this.state.hash !== hash) {
-      this.controller.loadBlock(hash);
-      this.setState({
-        hash,
-      });
+      this.loadData(hash);
     }
   }
 
-  handleBlock({ block, nextBlock, prevBlock }) {
-    this.setState({
-      block,
-      nextBlock,
-      prevBlock,
-    });
-  }
-
-  handleFailure(err) {
-    this.setState({
-      block: undefined,
-      nextBlock: undefined,
-      prevBlock: undefined,
-    });
+  async loadData(hash) {
+    try {
+      this.setState({ loading: true, error: false });
+      const [{ block }, { blockchain }] = await Promise.all([
+        getBlockInfo(hash),
+        getBlockchainInfo(),
+      ]);
+      const nextBlock = block.number > 0 ? block.number - 1 : -1;
+      const prevBlock = block.number < blockchain.blockNumber ? block.number + 1 : -1;
+      const data = {
+        hash,
+        block,
+        nextBlock,
+        prevBlock,
+      };
+      this.setState({ loading: false, error: false, data });
+    } catch (ex) {
+      this.setState({ loading: false, error: true });
+    }
   }
 
   render() {
+    const {
+      loading,
+      data: {
+        block,
+        nextBlock = -1,
+        prevBlock = -1,
+      } = {},
+    } = this.state;
     return (
       <Grid>
         <Grid.Row>
@@ -65,14 +71,26 @@ class BlockInfo extends React.Component {
         </Grid.Row>
         <Grid.Row>
           <Grid.Column>
-            {this.state.prevBlock && <Link to={`/block/${this.state.prevBlock}`} className="button button-primary">Previous</Link>}
-            {this.state.prevBlock && '\u00a0'}
-            {this.state.nextBlock !== false && <Link to={`/block/${this.state.nextBlock}`} className="button button-primary">Next</Link>}
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column>
-            <BlockInfoComponent block={this.state.block} />
+            {loading && <Loader active inline size="tiny" />}
+            <Button.Group floated="right">
+              <Button
+                {...{ disabled: prevBlock < 0 }}
+                labelPosition="left"
+                content="Previous"
+                icon="left chevron"
+                as={Link}
+                to={`/block/${prevBlock}`}
+              />
+              <Button
+                {...{ disabled: nextBlock < 0 }}
+                labelPosition="right"
+                content="Next"
+                icon="right chevron"
+                as={Link}
+                to={`/block/${nextBlock}`}
+              />
+            </Button.Group>
+            <BlockInfoComponent block={block} />
           </Grid.Column>
         </Grid.Row>
       </Grid>
