263eca7f92b8bc509c5956a073561c7ea566ec44
Ismael Bejarano <ismaelbej@gmail.com>
2017-10-21 15:25:47 -0300

    Add transaction receipt info

--- client/src/components/TxInfo.js
+++ client/src/components/TxInfo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {
+  Tab,
   Table,
   TextArea,
 } from 'semantic-ui-react';
@@ -11,7 +12,7 @@ import {
 
 const NULL_HASH = '0x0000000000000000000000000000000000000000000000000000000000000000';
 
-const TxInfo = (props) => {
+const InfoTab = (props) => {
   const { tx } = props;
   if (!tx) {
     return <div />;
@@ -51,7 +52,7 @@ const TxInfo = (props) => {
           <Table.Cell>To:</Table.Cell>
           {tx.to && <Table.Cell><Link to={`/account/${tx.to}`}>{tx.to}</Link></Table.Cell>}
           {!tx.to && tx.receipt.contractAddress && <Table.Cell>Contract created <Link to={`/account/${tx.receipt.contractAddress}`}>{tx.receipt.contractAddress}</Link></Table.Cell>}
-          {!tx.to && !tx.receipt.contractAddress && <Table.Cell>Contract creation faild</Table.Cell>}
+          {!tx.to && !tx.receipt.contractAddress && <Table.Cell>Contract creation failed</Table.Cell>}
         </Table.Row>
         <Table.Row>
           <Table.Cell>Gas:</Table.Cell>
@@ -81,6 +82,72 @@ const TxInfo = (props) => {
         </Table.Row>
       </Table.Body>
     </Table>
+  );
+};
+
+const ReceiptTab = (props) => {
+  const { tx: { receipt } = {} } = props;
+  if (!receipt) {
+    return <div />;
+  }
+  return (
+    <Table>
+      <Table.Body>
+        <Table.Row>
+          <Table.Cell>Hash:</Table.Cell>
+          <Table.Cell>{receipt.transactionHash}</Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>Block Hash:</Table.Cell>
+          <Table.Cell><Link to={`/block/${receipt.blockHash}`}>{receipt.blockHash}</Link></Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>Block Number:</Table.Cell>
+          <Table.Cell><Link to={`/block/${receipt.blockHash}`}>{receipt.blockNumber}</Link></Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>Transaction Index:</Table.Cell>
+          <Table.Cell>{receipt.transactionIndex}</Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>From:</Table.Cell>
+          <Table.Cell><Link to={`/account/${receipt.from}`}>{receipt.from}</Link></Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>To:</Table.Cell>
+          {receipt.to && <Table.Cell><Link to={`/account/${receipt.to}`}>{receipt.to}</Link></Table.Cell>}
+          {!receipt.to && <Table.Cell>Contract creation</Table.Cell>}
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>Contract Address:</Table.Cell>
+          {receipt.contractAddress && <Table.Cell><Link to={`/contract/${receipt.contractAddress}`}>{receipt.contractAddress}</Link></Table.Cell>}
+          {!receipt.contractAddress && <Table.Cell>null</Table.Cell>}
+        </Table.Row>
+        {receipt.status && <Table.Row>
+          <Table.Cell>Status:</Table.Cell>
+          {receipt.status === '0x1' && <Table.Cell>Succeeded</Table.Cell>}
+          {receipt.status === '0x0' && <Table.Cell>Failed</Table.Cell>}
+        </Table.Row>}
+        <Table.Row>
+          <Table.Cell>Cumulative Gas Used:</Table.Cell>
+          <Table.Cell>{receipt.cumulativeGasUsed}</Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>Gas Used:</Table.Cell>
+          <Table.Cell>{receipt.gasUsed}</Table.Cell>
+        </Table.Row>
+      </Table.Body>
+    </Table>
+  );
+};
+
+const TxInfo = (props) => {
+  const panes = [
+    { menuItem: 'Transaction', render: () => <InfoTab {...props} /> },
+    { menuItem: 'Receipt', render: () => <ReceiptTab {...props} /> },
+  ];
+  return (
+    <Tab menu={{ pointing: true }} panes={panes} />
   );
 };
 
