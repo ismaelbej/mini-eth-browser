2453ac718c54075cad98cf6fb7335ec6f5bef317
Ismael Bejarano <ismaelbej@gmail.com>
2017-11-04 02:27:43 -0300

    Make backend url configurable

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,32 +1,34 @@
 import fetch from 'unfetch';
 import { writeUrl } from '../utils/urlParams';
+import config from '../config';
+
 
 function fetchJson(url, params) {
   return fetch(writeUrl(url, params)).then(r => r.json());
 }
 
 export function getAccountInfo(address) {
-  return fetchJson(`http://localhost:3001/api/v1/account/${address}`);
+  return fetchJson(`${config.rpcpath}/api/v1/account/${address}`);
 }
 
 export function getBlockchainInfo() {
-  return fetchJson('http://localhost:3001/api/v1/blockchain');
+  return fetchJson(`${config.rpcpath}/api/v1/blockchain`);
 }
 
 export function getBlockInfo(hash) {
-  return fetchJson(`http://localhost:3001/api/v1/block/${hash}`);
+  return fetchJson(`${config.rpcpath}/api/v1/block/${hash}`);
 }
 
 export function getBlockList(start, count) {
-  return fetchJson('http://localhost:3001/api/v1/block/', { start, count });
+  return fetchJson(`${config.rpcpath}/api/v1/block/`, { start, count });
 }
 
 export function getTransactionInfo(txid) {
-  return fetchJson(`http://localhost:3001/api/v1/tx/${txid}`);
+  return fetchJson(`${config.rpcpath}/api/v1/tx/${txid}`);
 }
 
 export function getTransactionList(block, start, count) {
-  return fetchJson(`http://localhost:3001/api/v1/block/${block}/txs/`, { start, count });
+  return fetchJson(`${config.rpcpath}/api/v1/block/${block}/txs/`, { start, count });
 }
 
 export default {
