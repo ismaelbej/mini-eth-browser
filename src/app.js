fca35f442695eef615dc9d325c3b056c7fb6b590
Ismael Bejarano <ismaelbej@gmail.com>
2017-11-04 16:48:41 -0300

    Simplify launcher

--- src/app.js
+++ src/app.js
@@ -1,32 +1,42 @@
 import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
-
+import http from 'http';
+import config from './config';
 import api from './routes/api';
 
-const app = express();
+function createApp() {
+  const app = express();
 
-app.use(logger('dev'));
-app.use(cors());
+  app.use(logger('dev'));
+  app.use(cors());
 
-app.use('/api/v1', api);
+  app.use('/api/v1', api);
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  // catch 404 and forward to error handler
+  app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
 
-// error handler
-app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // error handler
+  app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json({ errors: [err.message] });
-});
+    // render the error page
+    res.status(err.status || 500);
+    res.json({ errors: [err.message] });
+  });
 
-export default app;
+  return app;
+}
+
+function createServer(app) {
+  const server = http.createServer(app);
+  server.listen(config.port || 3000);
+}
+
+createServer(createApp());
