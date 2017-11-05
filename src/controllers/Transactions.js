a241a65c0b05602e78be450a7029c5d5468e5473
Ismael Bejarano <ismaelbej@gmail.com>
2017-11-05 00:28:48 -0300

    Do not use a class when not needed

--- src/controllers/Transactions.js
+++ src/controllers/Transactions.js
@@ -5,38 +5,37 @@ import {
   getTransactionReceipt,
 } from '../lib/ethereum';
 
-class TransactionController {
-  // eslint-disable-next-line class-methods-use-this
-  async get(txid) {
-    try {
-      const [tx, receipt] = await Promise.all([
-        getTransaction(txid),
-        getTransactionReceipt(txid),
-      ]);
-      const block = receipt ? await getBlock(tx.blockHash) : null;
-      if (block) {
-        tx.block = block;
-      }
-      if (receipt) {
-        tx.receipt = receipt;
-      }
-      if (tx.to) {
-        const code = await getCode(tx.to);
-        if (code && code !== '0x0') {
-          tx.code = code;
-        }
-      }
-      return tx;
-    } catch (ex) {
-      console.log(`${ex.stack}`);
+export async function getTransactionInfo(txid) {
+  try {
+    const [tx, receipt] = await Promise.all([
+      getTransaction(txid),
+      getTransactionReceipt(txid),
+    ]);
+    const block = receipt ? await getBlock(tx.blockHash) : null;
+    if (block) {
+      tx.block = block;
     }
-    return {};
+    if (receipt) {
+      tx.receipt = receipt;
+    }
+    if (tx.to) {
+      const code = await getCode(tx.to);
+      if (code && code !== '0x0') {
+        tx.code = code;
+      }
+    }
+    return tx;
+  } catch (ex) {
+    console.log(`${ex.stack}`);
   }
-
-  // eslint-disable-next-line class-methods-use-this
-  list() {
-    return [];
-  }
+  return {};
 }
 
-export default new TransactionController();
+export function listTransactions() {
+  return [];
+}
+
+export default {
+  getTransactionInfo,
+  listTransactions,
+};
--- src/routes/tx.js
+++ src/routes/tx.js
@@ -1,11 +1,28 @@
 import express from 'express';
-import Transactions from '../controllers/Transactions';
+import {
+  getTransactionInfo,
+  listTransactions,
+} from '../controllers/Transactions';
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+  try {
+    const txs = await listTransactions();
+    res.json({
+      txs,
+    });
+  } catch (err) {
+    res.status(err.status || 500);
+    res.json({
+      errors: [err.message],
+    });
+  }
+});
+
 router.get('/:txid', async (req, res) => {
   try {
-    const tx = await Transactions.get(req.params.txid);
+    const tx = await getTransactionInfo(req.params.txid);
     res.json({
       tx,
     });
