544779dde80f8eca50da2acfc3526eeeb11e36bd
Ismael Bejarano <ismaelbej@gmail.com>
2017-11-05 16:58:26 -0300

    Errors will not propagate

--- src/controllers/Transactions.js
+++ src/controllers/Transactions.js
@@ -6,29 +6,24 @@ import {
 } from '../lib/ethereum';
 
 export async function getTransactionInfo(txid) {
-  try {
-    const [tx, receipt] = await Promise.all([
-      getTransaction(txid),
-      getTransactionReceipt(txid),
-    ]);
-    const block = receipt ? await getBlock(tx.blockHash) : null;
-    if (block) {
-      tx.block = block;
-    }
-    if (receipt) {
-      tx.receipt = receipt;
-    }
-    if (tx.to) {
-      const code = await getCode(tx.to);
-      if (code && code !== '0x0') {
-        tx.code = code;
-      }
-    }
-    return tx;
-  } catch (ex) {
-    console.log(`${ex.stack}`);
+  const [tx, receipt] = await Promise.all([
+    getTransaction(txid),
+    getTransactionReceipt(txid),
+  ]);
+  const block = receipt ? await getBlock(tx.blockHash) : null;
+  if (block) {
+    tx.block = block;
   }
-  return {};
+  if (receipt) {
+    tx.receipt = receipt;
+  }
+  if (tx.to) {
+    const code = await getCode(tx.to);
+    if (code && code !== '0x0') {
+      tx.code = code;
+    }
+  }
+  return tx;
 }
 
 export function listTransactions() {
