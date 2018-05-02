fbaa2dc0cb888eb454cf0aaa8fd8527acc426a63
Ismael Bejarano <ismael.bejarano@coinfabrik.com>
2018-05-02 14:02:35 -0300

    Autorefresh for transactions list

--- client/src/views/TxList.js
+++ client/src/views/TxList.js
@@ -7,6 +7,7 @@ import {
 import queryString from 'query-string';
 import TxListComponent from '../components/TxList';
 import PrevNext from '../components/PrevNext';
+import AutoRefresh from '../components/AutoRefresh';
 import {
   getBlockchainInfo,
   getTransactionList,
@@ -14,6 +15,30 @@ import {
 
 const BLOCK_COUNT = 20;
 const TXLIST_REFRESH_TIMEOUT = 10;
+
+const TxListView = ({ loading, data: { prevBlock = -1, nextBlock = -1, count, txs = [] } = {} }) => (
+  <Grid>
+    <Grid.Row>
+      <Grid.Column>
+        <Header as="h1">Transactions</Header>
+      </Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column>
+        {loading && <Loader active inline size="tiny" />}
+        <PrevNext
+          hasPrev={prevBlock >= 0}
+          prev={`/tx/?start=${prevBlock}&count=${count}`}
+          hasNext={nextBlock >= 0}
+          next={`/tx/?start=${nextBlock}&count=${count}`}
+        />
+        <TxListComponent txs={txs} />
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+);
+
+const TxListViewRefresh = AutoRefresh(TxListView, TXLIST_REFRESH_TIMEOUT * 1000);
 
 function parseParams(props) {
   let { start, count } = queryString.parse(props.location.search);
@@ -37,13 +62,12 @@ class TxList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.refreshEvents = this.refreshEvents.bind(this);
+    this.refreshView = this.refreshView.bind(this);
   }
 
   componentDidMount() {
     const { start, count } = parseParams(this.props);
     this.loadData(start, count);
-    setTimeout(this.refreshEvents, TXLIST_REFRESH_TIMEOUT * 1000);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -74,46 +98,16 @@ class TxList extends React.Component {
     }
   }
 
-  async refreshEvents() {
+  refreshView() {
     const { start, count } = parseParams(this.props);
     if (!this.state.data || this.state.data.start !== start ||
         this.state.data.count !== count) {
       this.loadData(start, count);
     }
-    setTimeout(this.refreshEvents, TXLIST_REFRESH_TIMEOUT * 1000);
   }
 
   render() {
-    const {
-      loading,
-      data: {
-        count,
-        txs = [],
-        nextBlock = -1,
-        prevBlock = -1,
-      } = {},
-    } = this.state;
-    return (
-      <Grid>
-        <Grid.Row>
-          <Grid.Column>
-            <Header as="h1">Transactions</Header>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column>
-            {loading && <Loader active inline size="tiny" />}
-            <PrevNext
-              hasPrev={prevBlock >= 0}
-              prev={`/tx/?start=${prevBlock}&count=${count}`}
-              hasNext={nextBlock >= 0}
-              next={`/tx/?start=${nextBlock}&count=${count}`}
-            />
-            <TxListComponent txs={txs} />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    );
+    return <TxListViewRefresh {...this.state} refreshView={this.refreshView} />;
   }
 }
 
