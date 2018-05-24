c16331579c96a2313bd4bf2760e3384729b83a75
Ismael Bejarano <ismael.bejarano@coinfabrik.com>
2018-05-24 17:06:30 -0300

    Add elapsed time, and address created contract

--- client/src/components/TxList.js
+++ client/src/components/TxList.js
@@ -7,6 +7,7 @@ import {
   formatAddress,
   formatAmount,
   formatHash,
+  formatElapsed,
 } from '../utils/formatters';
 
 const TxList = (props) => {
@@ -21,6 +22,7 @@ const TxList = (props) => {
           <Table.HeaderCell>From</Table.HeaderCell>
           <Table.HeaderCell>To</Table.HeaderCell>
           <Table.HeaderCell>Value</Table.HeaderCell>
+          <Table.HeaderCell>Timestamp</Table.HeaderCell>
           <Table.HeaderCell>Gas</Table.HeaderCell>
         </Table.Row>
       </Table.Header>
@@ -32,8 +34,10 @@ const TxList = (props) => {
             <Table.Cell>{tx.transactionIndex}</Table.Cell>
             <Table.Cell><Link to={`/account/${tx.from}`}>{formatAddress(tx.from)}</Link></Table.Cell>
             {tx.to && <Table.Cell><Link to={`/account/${tx.to}`}>{formatAddress(tx.to)}</Link></Table.Cell>}
-            {!tx.to && <Table.Cell>Contract creation</Table.Cell>}
+            {!tx.to && tx.receipt.status !== '0x00' && <Table.Cell>(Created <Link to={`/account/${tx.receipt.contractAddress}`}>{formatAddress(tx.receipt.contractAddress, 12)}</Link>)</Table.Cell>}
+            {!tx.to && tx.receipt.status === '0x00' && <Table.Cell>(Creation failed)</Table.Cell>}
             <Table.Cell>{formatAmount(tx.value)}</Table.Cell>
+            <Table.Cell>{formatElapsed(tx.block.timestamp)}</Table.Cell>
             <Table.Cell textAlign="right" >{tx.receipt.gasUsed}</Table.Cell>
           </Table.Row>
         ))}
