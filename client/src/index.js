1e3b8df30ed30ab148047a39a1ab31c671497024
Ismael Bejarano <ismaelbej@gmail.com>
2018-10-15 00:42:01 -0300

    Async component

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,23 @@
 import { app, h } from 'hyperapp';
 
-const actions = {};
+const actions = {
+  updateBlockchain: () => async ({block, gasPrice}, actions) => {
+    setTimeout(actions.refreshStatus, 5000, {block, gasPrice: gasPrice + 1});
+  },
+  refreshStatus: ({block, gasPrice}) => (state, actions) => {
+    return { block, gasPrice };
+  }
+};
 
 const state = {
   block: {},
   gasPrice: 0,
 };
 
-const view = ({block, gasPrice}) => (
-  <div>
+const view = ({block, gasPrice}, {updateBlockchain}) => (
+  <div
+    oncreate={() => updateBlockchain()}
+  >
     <section>
       <ul>
         <li>Block: {block.timestamp}</li>
