d6a3e11bf6d7e28cb4e4d2b627d0df1826e421a6
Ismael Bejarano <ismaelbej@gmail.com>
2018-10-15 01:05:36 -0300

    Fix eslint warnings

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
 import { app, h } from 'hyperapp';
 
 const actions = {
-  updateBlockchain: () => async ({block, gasPrice}, actions) => {
-    setTimeout(actions.refreshStatus, 5000, {block, gasPrice: gasPrice + 1});
+  updateBlockchain: () => async ({ block, gasPrice }, { refreshStatus }) => {
+    setTimeout(refreshStatus, 5000, { block, gasPrice: gasPrice + 1 });
   },
-  refreshStatus: ({block, gasPrice}) => (state, actions) => {
-    return { block, gasPrice };
-  }
+  refreshStatus: ({ block, gasPrice }) => () => ({
+    block,
+    gasPrice,
+  }),
 };
 
 const state = {
@@ -14,18 +15,23 @@ const state = {
   gasPrice: 0,
 };
 
-const view = ({block, gasPrice}, {updateBlockchain}) => (
+const view = ({ block, gasPrice }, { updateBlockchain }) => (
   <div
     oncreate={() => updateBlockchain()}
   >
     <section>
       <ul>
-        <li>Block: {block.timestamp}</li>
-        <li>Gas Price: {gasPrice}</li>
+        <li>
+          Block:
+          {block.timestamp}
+        </li>
+        <li>
+          Gas Price:
+          {gasPrice}
+        </li>
       </ul>
     </section>
   </div>
 );
 
 app(state, actions, view, document.getElementById('app'));
-
